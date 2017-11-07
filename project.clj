13fa8784c73c50b6fdcc67c8339215d9a0e8dab9
Daniel Compton <desk@danielcompton.net>
2017-11-08 11:17:13 +1300

    Update dependencies

--- project.clj
+++ project.clj
@@ -7,11 +7,11 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
-                 [ring/ring-defaults "0.1.5"]
-                 [ring/ring-core "1.4.0"]
-                 [ring-server "0.4.0"]
+                 [ring/ring-defaults "0.3.1"]
+                 [ring/ring-core "1.6.3"]
+                 [ring-server "0.5.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-jetty-adapter "1.6.3"]
                  [ancient-clj "0.3.11"]
                  [ring/ring-json "0.4.0"]
                  [slingshot/slingshot "0.12.2"]
@@ -26,6 +26,6 @@
             [jonase/eastwood "0.2.1"]
             [lein-kibit "0.1.2"]]
   :profiles {:dev  {:dependencies [[ring-mock "0.1.5"]
-                                   [ring/ring-devel "1.4.0"]]
+                                   [ring/ring-devel "1.6.3"]]
                     :ring {:open-browser? true :stacktraces? true :auto-reload? true :port 3002}}
              :prod {:ring {:open-browser? false, :stacktraces? false, :auto-reload? false}}})
