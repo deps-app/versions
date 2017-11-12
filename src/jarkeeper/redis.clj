2963793ac196884dfd479b0a42687142c5bbda52
Daniel Compton <desk@danielcompton.net>
2017-11-13 10:18:03 +1300

    Setup access to Redis

---
+++ src/jarkeeper/redis.clj
@@ -0,0 +1,6 @@
+(ns jarkeeper.redis
+  (:require [taoensso.carmine :as car :refer [wcar]]
+            [environ.core :refer [env]]))
+
+(def server1-conn {:pool {} :spec {:uri (:redis-url env)}}) ; See `wcar` docstring for opts
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
