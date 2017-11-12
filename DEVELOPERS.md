29a2ce2916e79e6b5e1c6a0b85e14074f0c43d2a
Daniel Compton <desk@danielcompton.net>
2017-11-13 11:01:46 +1300

    Enable connection pool

--- DEVELOPERS.md
+++ DEVELOPERS.md
@@ -5,3 +5,11 @@
 ```console
 $ lein ring server
 ```
+
+## Heroku
+
+Heroku Redis maxmemory-policy is `volatile-lru`
+
+```console
+$ heroku redis:maxmemory --policy volatile-lru
+```
--- src/jarkeeper/redis.clj
+++ src/jarkeeper/redis.clj
@@ -2,5 +2,5 @@
   (:require [taoensso.carmine :as car :refer [wcar]]
             [environ.core :refer [env]]))
 
-(def server1-conn {:pool {} :spec {:uri (:redis-url env)}}) ; See `wcar` docstring for opts
+(def server1-conn {:spec {:uri (:redis-url env)}}) ; See `wcar` docstring for opts
 (defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
