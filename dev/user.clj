b06e12b7b46f354d6c4b1091253ae48f8bf34955
Александар Симић <asimic@gmail.com>
2017-12-13 15:13:52 +0100

    Sorted the requires alphabetically
    Used cljr-clean-ns, but left most of the functions as they'll be used
    in the REPL

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,5 @@
 (ns user
-  (:require [reloaded.repl :refer [system init start stop go reset reset-all]]
-            [jarkeeper.core :refer [config new-system]]))
+  (:require [jarkeeper.core :refer [config new-system]]
+            [reloaded.repl :refer [system init start stop go reset reset-all]]))
 
 (reloaded.repl/set-init! #(new-system :dev))
