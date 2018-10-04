ba494ae960c640a646fd637b37ba3c18a3a89754
Александар Симић <asimic@gmail.com>
2018-10-04 23:32:15 +0100

    Fixed the broken test
    See the broken test run:
    
    https://circleci.com/gh/deps-app/versions/66
    
    this change fixes it and makes the build green again.

--- test/jarkeeper/web_test.clj
+++ test/jarkeeper/web_test.clj
@@ -7,12 +7,12 @@
   (testing "png image"
     (is (= (img-status-resp "public/images/out-of-date.png")
            (-> (resp/resource-response "public/images/out-of-date.png")
-               (resp/header "cache-control" "no-cache")
+               (resp/header "cache-control" "max-age=300, stale-while-revalidate=30")
                (resp/header "last-modified" (last-modified))
                (resp/header "content-type" "image/png")))))
   (testing "svg image"
     (is (= (img-status-resp "public/images/out-of-date.svg")
            (-> (resp/resource-response "public/images/out-of-date.svg")
-               (resp/header "cache-control" "no-cache")
+               (resp/header "cache-control" "max-age=300, stale-while-revalidate=30")
                (resp/header "last-modified" (last-modified))
                (resp/header "content-type" "image/svg+xml"))))))
